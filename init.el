fbbd8abdda7ccde50aa90bbdd884e62a4067faca
henm <henm@users.noreply.github.com>
2020-04-25 07:09:36 +0900

    Initial commit

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+(setq use-package-always-ensure t)
+
+(use-package org)
+(use-package racket-mode)
+
+(global-display-line-numbers-mode)
+(menu-bar-mode -1)
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
