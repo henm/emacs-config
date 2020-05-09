21f6e5bda8cd3fe30b1e18f3f870affaeeba9ba6
henm <henm@users.noreply.github.com>
2020-05-09 11:36:37 +0900

    Use winum; use treemacs

--- init.el
+++ init.el
@@ -11,8 +11,38 @@
 (use-package org)
 (use-package racket-mode)
 
+(use-package winum
+  :bind (("C-w" . winum-select-window-by-number)
+         ("M-0" . winum-select-window-0-or-10)
+         ("M-1" . winum-select-window-1)
+         ("M-2" . winum-select-window-2)
+         ("M-3" . winum-select-window-3)
+         ("M-4" . winum-select-window-4)
+         ("M-5" . winum-select-window-5)
+         ("M-6" . winum-select-window-6)
+         ("M-7" . winum-select-window-7)
+         ("M-8" . winum-select-window-8)))
+(winum-mode)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :bind (("M-0"       . treemacs-select-window)
+         ("C-x t t"   . treemacs)))
+(treemacs)
+
+(use-package lsp-mode :ensure t)
+(use-package hydra :ensure t)
+(use-package lsp-ui :ensure t)
+(use-package lsp-java :ensure t
+  :config (add-hook 'java-mode-hook 'lsp))
+
 (global-display-line-numbers-mode)
 (menu-bar-mode -1)
+(tool-bar-mode -1)
 (setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
 
 (custom-set-variables
